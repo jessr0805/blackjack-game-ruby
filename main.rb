59fd9582844e4b6063ba7c13871d2eec6df4c9e6
jessr0805 <jessesodhi13@gmail.com>
2026-08-19 19:54:28 -0400

    feat: shuffle deck and deal opening hands

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 deck = []
+player_hand = []
+dealer_hand = []
 suits.each do |suit|
     ranks.each do |rank|
         new_card = {
@@ -10,3 +12,14 @@ suits.each do |suit|
         deck << new_card
     end
 end
+shuffled_deck = deck.shuffle
+card_dealt = shuffled_deck.pop
+player_hand << card_dealt
+card_dealt = shuffled_deck.pop
+dealer_hand << card_dealt
+card_dealt = shuffled_deck.pop
+player_hand << card_dealt
+card_dealt = shuffled_deck.pop
+dealer_hand << card_dealt
+puts player_hand
+puts dealer_hand
